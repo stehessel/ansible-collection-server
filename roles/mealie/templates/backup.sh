0b2514cabda96133707c421725588505eb73b064
stehessel <stephan@stehessel.de>
2022-12-27 23:42:11 +0100

    remove container from cmd

--- roles/mealie/templates/backup.sh
+++ roles/mealie/templates/backup.sh
@@ -5,5 +5,5 @@ set -o nounset   # abort on unbound variable
 set -o pipefail  # don't hide errors within pipes
 
 /usr/bin/docker container stop mealie-api
-/usr/bin/docker container cp mealie-api:/app/data {{ mealie__path }}/backup
+/usr/bin/docker cp mealie-api:/app/data {{ mealie__path }}/backup
 /usr/bin/docker container start mealie-api
